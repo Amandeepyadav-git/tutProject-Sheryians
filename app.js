49b434a7900f6fd024fe00b7c9f94889413b41a3
Amandeep Yadav <amandeepyadav0704@gmail.com>
2025-11-29 13:04:31 +0530

    hello this is after updating to save the posts of user in differend id and passwords

--- app.js
+++ app.js
@@ -21,12 +21,11 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/profile", isLoggedIn, async (req, res) => {
-  let user = await userModel.findOne({email: req.user.email})
-  console.log(user);  
+  let user = await userModel.findOne({email: req.user.email}).populate("posts")
   res.render("profile",{user});
 });
 
-app.get("/post", isLoggedIn, async (req, res) => {
+app.post("/post", isLoggedIn, async (req, res) => {
   let user = await userModel.findOne({email: req.user.email})
   let {content} = req.body;
 let post = await postModel.create({
@@ -34,7 +33,9 @@ let post = await postModel.create({
     content,
   });
 
-  user.post.push(post_id)
+  user.posts.push(post._id)
+  await user.save()
+  res.redirect("/profile")
 });
 
 
